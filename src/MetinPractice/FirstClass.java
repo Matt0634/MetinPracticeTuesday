b14a8c5952927baeafa619bf271bed2e0957000e
metoceto <57574169+metoceto@users.noreply.github.com>
2020-01-26 21:08:22 -0600

    Second push

--- src/MetinPractice/FirstClass.java
+++ src/MetinPractice/FirstClass.java
@@ -3,5 +3,8 @@ package MetinPractice;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Today is Sunday!");
+        System.out.println("Everything will be  better in the future!");
+
     }
 }
