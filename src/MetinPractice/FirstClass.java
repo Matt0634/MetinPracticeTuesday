db4a4f80fa8def1f7610524877e3132cea6a5700
metoceto <57574169+metoceto@users.noreply.github.com>
2020-01-26 21:19:28 -0600

    Third push

--- src/MetinPractice/FirstClass.java
+++ src/MetinPractice/FirstClass.java
@@ -5,6 +5,10 @@ public class FirstClass {
         System.out.println("Hello world!");
         System.out.println("Today is Sunday!");
         System.out.println("Everything will be  better in the future!");
+        
+        System.out.println("Today is Sunday!");
+        System.out.println("Everything will be  better in the future!");
+
 
     }
 }
